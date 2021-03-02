f1e8bbc6aef993fbb445fdebb8d3bd4e85b5e8b5
TheDusKnight <860956020@qq.com>
2021-03-01 21:28:50 -0800

    begin frontend

---
+++ flask_server/static/index.js
@@ -0,0 +1,10 @@
+'use strict';
+// alert("listen!")
+console.log('window inner sizes: ' + window.innerWidth + ' x ' + window.innerHeight);
+// console.log('appName = ' + navigator.appName);
+// console.log('appVersion = ' + navigator.appVersion);
+// console.log('language = ' + navigator.language);
+// console.log('platform = ' + navigator.platform);
+// console.log('userAgent = ' + navigator.userAgent);
+
+document.title = '学习java'
