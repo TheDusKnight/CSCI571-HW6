8cf42f661824d6d02114d2a9d146b2d5b6a1fdcd
TheDusKnight <860956020@qq.com>
2021-03-03 00:21:22 -0800

    重新开始slide show

--- flask_server/static/index.js
+++ flask_server/static/index.js
@@ -23,62 +23,21 @@ async function fetchR(inputLnk) {
     method: 'GET',
     // TODO: 在server和browser添加cor功能
     // mode:'cors',
-    // responseType: 'json',
+    responseType: 'json',
   });
-  let result = await response.text() // TODO: 为什么要有text?
+  let result = await response.text()
   // alert(result)
   console.log(result)
   // document.getElementById('trending_movie').innerHTML = result
   return result
 }
-// fetchR("http://localhost:5000/api/movies").then(result=>display_trending(result, "slides", "movie"))
+fetchR("http://localhost:5000/api/movies").then(result=>display_trending(result, 'slides'))
 var slideIndex = 0
-display_trending_movie()
-// trending_movie()
+// showSlides()
 
 // Display AJAX result in html
-function display_trending(result, id, type) {
-  // alert(typeof result)
-  // document.getElementById(id).innerHTML = result
-
-  const obj = JSON.parse(result)
-  if (type === "movie") {
-    // trending_movie()
-    console.log("nothing")
-  } else if (id === "tv") {
-    
-  }
-
-  // for (var i in obj.results) {
-  //   console.log(obj.results[i].title)
-  // }
-  // console.log(obj.results[val].title);
-  // var info = {
-  //   imgPath = result.backdrop_path,
-  //   year = result.release_date,
-  //   title = result.title
-  // }
-  // alert(JSON)
-
-  // var i;
-  // var slides = document.getElementsByClassName(id);
-  // console.log(slides)
-  // var captions = document.getElementsByClassName("captions");
-  // for (i = 0; i < slides.length; i++) {
-  //   slides[i].style.display = "none";
-  // }
-  // slideIndex++;
-  // if (slideIndex > slides.length) {slideIndex = 1} // 恢复index
-  // slides[slideIndex-1].style.display = "block";
-
-  // alert(obj.results[slideIndex-1].backdrop_path)
-  // slides[slideIndex-1].src = obj.results[slideIndex-1].backdrop_path
-  // console.log(obj.results[slideIndex-1].backdrop_path)
-  
-  // setTimeout(display_trending(id), 3000); // Change image every 2 seconds
-}
-
-function display_trending_movie(result,id) {
+function display_trending(result,id) {
+  // document.getElementById(id).src = result
   var i;
   var slides = document.getElementsByClassName("slides");
   var captions = document.getElementsByClassName("captions");
@@ -88,24 +47,5 @@ function display_trending_movie(result,id) {
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1} // 恢复index
   slides[slideIndex-1].style.display = "block";
-  setTimeout(display_trending_movie, 3000); // Change image every 2 seconds
-}
-
-function trending_movie() {
-  var i;
-  var slides = document.getElementsByClassName("slides");
-  console.log(slides)
-  var captions = document.getElementsByClassName("captions");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1} // 恢复index
-  slides[slideIndex-1].style.display = "block";
-  // alert(obj.results[slideIndex-1].backdrop_path)
-
-  // slides[slideIndex-1].src = obj.results[slideIndex-1].backdrop_path
-  // console.log(obj.results[slideIndex-1].backdrop_path)
-  
-  setTimeout(trending_movie, 3000); // Change image every 2 seconds
+  setTimeout(display_trending, 3000); // Change image every 3 seconds
 }
