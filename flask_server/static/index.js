2ba0950ed9b090d09deae511108f8a4d7653bfea
TheDusKnight <860956020@qq.com>
2021-03-02 16:40:09 -0800

    清理html comments

--- flask_server/static/index.js
+++ flask_server/static/index.js
@@ -1,10 +1,19 @@
 'use strict';
 // alert("listen!")
-console.log('window inner sizes: ' + window.innerWidth + ' x ' + window.innerHeight);
-// console.log('appName = ' + navigator.appName);
-// console.log('appVersion = ' + navigator.appVersion);
-// console.log('language = ' + navigator.language);
-// console.log('platform = ' + navigator.platform);
-// console.log('userAgent = ' + navigator.userAgent);
+console.log('window inner sizes: ' + window.innerWidth + ' x ' + window.innerHeight)
+// console.log('appName = ' + navigator.appName)
+// console.log('appVersion = ' + navigator.appVersion)
+// console.log('language = ' + navigator.language)
+// console.log('platform = ' + navigator.platform)
+// console.log('userAgent = ' + navigator.userAgent)
 
 document.title = '学习java'
+
+function toggleSearch() {
+  document.getElementsByClassName('search').hidden = false
+  document.getElementsByClassName('home').hidden = true
+}
+function toggleHome() {
+  document.getElementsByClassName('search').hidden = true
+  document.getElementsByClassName('home').hidden = false
+}
