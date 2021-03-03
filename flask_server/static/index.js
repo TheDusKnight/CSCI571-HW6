48fdc87dfd6ab11f35829dd39a1cc66b1f9eb430
TheDusKnight <860956020@qq.com>
2021-03-02 22:35:59 -0800

    slidewindows成功，准备注入json数据

--- flask_server/static/index.js
+++ flask_server/static/index.js
@@ -21,6 +21,7 @@ function toggleHome() {
 async function fetchR(inputLnk) {
   const response = await fetch(inputLnk, {
     method: 'GET',
+    // TODO: 在server和browser添加cor功能
     // mode:'cors',
     // responseType: 'json',
   });
@@ -31,9 +32,26 @@ async function fetchR(inputLnk) {
   return result
 }
 fetchR("http://localhost:5000/api/movies").then(result=>display_trending_movie(result, 'trending_movie'))
+var slideIndex = 0
+showSlides()
 
 // Display AJAX result in html
 function display_trending_movie(result,id) {
   // alert(typeof result)
   // document.getElementById(id).innerHTML = result
+
+  var i;
+  var slides = document.getElementsByClassName("slides");
+  var captions = document.getElementsByClassName("captions");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1} // 恢复index
+  // for (i = 0; i < dots.length; i++) {
+  //   dots[i].className = dots[i].className.replace(" active", "");
+  // }
+  slides[slideIndex-1].style.display = "block";
+  // dots[slideIndex-1].className += " active";
+  setTimeout(display_trending_movie, 3000); // Change image every 2 seconds
 }
