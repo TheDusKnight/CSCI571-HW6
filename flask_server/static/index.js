da7317b125d8f86c52b7a2e02c7dd65a9b6617ab
TheDusKnight <860956020@qq.com>
2021-03-02 19:28:56 -0800

    toggle function work, 问了老江他也不知道footer怎么做

--- flask_server/static/index.js
+++ flask_server/static/index.js
@@ -1,19 +1,16 @@
 'use strict';
 // alert("listen!")
 console.log('window inner sizes: ' + window.innerWidth + ' x ' + window.innerHeight)
-// console.log('appName = ' + navigator.appName)
-// console.log('appVersion = ' + navigator.appVersion)
-// console.log('language = ' + navigator.language)
-// console.log('platform = ' + navigator.platform)
-// console.log('userAgent = ' + navigator.userAgent)
 
 document.title = '学习java'
 
 function toggleSearch() {
-  document.getElementsByClassName('search').hidden = false
-  document.getElementsByClassName('home').hidden = true
+  document.getElementById('home').style.display = "none"
+  document.getElementById("search").style.display="block";
+  console.log("show search")
 }
+
 function toggleHome() {
-  document.getElementsByClassName('search').hidden = true
-  document.getElementsByClassName('home').hidden = false
+  document.getElementById('search').style.display = "none"
+  document.getElementById('home').style.display = "block"
 }
