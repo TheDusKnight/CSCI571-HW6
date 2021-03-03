4087ca9f7d9805623ed2f6efc4e2847867c4e988
TheDusKnight <860956020@qq.com>
2021-03-03 04:04:07 -0800

    slide show part 1 finish

--- flask_server/static/index.js
+++ flask_server/static/index.js
@@ -15,9 +15,14 @@ function toggleHome() {
   document.getElementById('home').style.display = "block"
 }
 
+function sleep(ms) {
+  return new Promise(resolve => 
+      setTimeout(resolve, ms)
+  )
+}
+
 // AJAX
 // var lnkHome = "http://127.0.0.1:5000/api/movies"
-// var lnkHome = "http://localhost:5000/api/movies"
 async function fetchR(inputLnk) {
   const response = await fetch(inputLnk, {
     method: 'GET',
@@ -31,21 +36,74 @@ async function fetchR(inputLnk) {
   // document.getElementById('trending_movie').innerHTML = result
   return result
 }
-fetchR("http://localhost:5000/api/movies").then(result=>display_trending(result, 'slides'))
+
 var slideIndex = 0
-// showSlides()
+window.onload = fetchR("http://localhost:5000/api/movies").then(result=>display_trending(result, "slides", "movie"))
+// fetchR("http://localhost:5000/api/movies").then(result=>showSlides(result, "slides", "movie"))
+
+function display_trending(result, id, type) {
+  const obj = JSON.parse(result)
+  if (type === "movie") {
+    // document.getElementById("img_test").src = "https://image.tmdb.org/t/p/w780/srYya1ZlI97Au4jUYAktDe3avyA.jpg"
+    // var slides = document.getElementsByClassName("slides")
+    var captions = document.getElementsByClassName("caption")
+    var slides = document.getElementsByClassName("movieSlide")
+    var res = obj.results
+    
+    for (var i in res) {
+      var imgPath = res[i].backdrop_path
+      var name = res[i].title
+      var year = res[i].release_date
+      // alert(slides[i].src)
+      slides[i].src = imgPath
+      captions[i].innerHTML = name + " (" + year + ")"
+    }
+    console.log("Trending Movies changed")
+    // alert(document.getElementsByClassName("slides")[0].src)
+    showSlides()
+  } 
+  else if (id === "tv") {
+    
+  }
+}
 
 // Display AJAX result in html
-function display_trending(result,id) {
+function showSlides() {
   // document.getElementById(id).src = result
   var i;
   var slides = document.getElementsByClassName("slides");
-  var captions = document.getElementsByClassName("captions");
+  // alert(slides[0].src)
+  // var captions = document.getElementsByClassName("captions");
+
+  // if (slides) {alert(slides[i])}
+  // alert(slides[0].innerHTML)
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";  
   }
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1} // 恢复index
   slides[slideIndex-1].style.display = "block";
-  setTimeout(display_trending, 3000); // Change image every 3 seconds
+  setTimeout(showSlides, 3000); // Change image every 3 seconds
 }
+
+// function display_trending_2(result, id) {
+//   var slideIndex = 0
+//   var i
+//   var slides = document.getElementsByClassName("slides")
+//   var captions = document.getElementsByClassName("captions")
+
+//   while (true) {
+//     for (i = 0; i < slides.length; i++) {
+//       slides[i].style.display = "none"
+//     }
+
+//     slideIndex++
+//     if (slideIndex >= slides.length) {slideIndex = 0}
+//     slides[slideIndex].style.display = "block"
+//     delay()
+//   }
+
+//   function delay() {
+//     setTimeout(3000)
+//   }
+// }
